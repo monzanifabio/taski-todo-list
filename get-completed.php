185a31486fe276a82b2fb26416f60110c435c655
Fabio Monzani <renderbros@gmail.com>
2020-06-06 21:50:01 +0100

    ajax update

--- get-completed.php
+++ get-completed.php
@@ -2,7 +2,7 @@
 // Include config file
 require_once "config.php";
 
-$user_id = $_SESSION['id'];
+$user_id = $_GET['user_id'];
 
 //running SQL query
 $query ="SELECT * FROM todos WHERE user_id = $user_id AND completed = '1'" ;
@@ -17,7 +17,7 @@ if ($isempty == 0) {
 //processing results
 while($row = mysqli_fetch_assoc($result)) {
     echo "<li class='list-group-item completed'>";
-    echo "<a href='undo-todo.php?todo_id=" . $row['todo_id'] . "' class='button-undo fas fa-undo-alt'></a>";
+    echo "<a onclick='undoTodo(this)' id='" . $row['todo_id'] . "' class='button-undo fas fa-undo-alt'></a>";
     echo "<p class='text-crossed'>" . $row['todo'] . "</p>";
     echo "</li>";
   }
