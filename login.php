0bbdb0a35c8ee1cd828953d3c1a9e7c589b9c5be
Fabio Monzani <renderbros@gmail.com>
2020-06-09 12:50:49 +0100

    Trying to get the login to work with ajax

--- login.php
+++ login.php
@@ -1,100 +1,11 @@
-<?php
-// Initialize the session
-session_start();
-
-// Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: index.php");
-    exit;
-}
-
-// Include config file
-require_once "config/config.php";
-
-// Define variables and initialize with empty values
-$username = $password = "";
-$username_err = $password_err = "";
-
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-
-    // Check if username is empty
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter username.";
-    } else{
-        $username = trim($_POST["username"]);
-    }
-
-    // Check if password is empty
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter your password.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
-
-    // Validate credentials
-    if(empty($username_err) && empty($password_err)){
-        // Prepare a select statement
-        $sql = "SELECT user_id, username, password FROM users WHERE username = ?";
-
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-
-            // Set parameters
-            $param_username = $username;
-
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Store result
-                mysqli_stmt_store_result($stmt);
-
-                // Check if username exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1){
-                    // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-                    if(mysqli_stmt_fetch($stmt)){
-                        if(password_verify($password, $hashed_password)){
-                            // Password is correct, so start a new session
-                            session_start();
-
-                            // Store data in session variables
-                            $_SESSION["loggedin"] = true;
-                            $_SESSION["id"] = $id;
-                            $_SESSION["username"] = $username;
-
-                            // Redirect user to welcome page
-                            header("location: index.php");
-                        } else{
-                            // Display an error message if password is not valid
-                            $password_err = "The password you entered was not valid.";
-                        }
-                    }
-                } else{
-                    // Display an error message if username doesn't exist
-                    $username_err = "No account found with that username.";
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-
-            // Close statement
-            mysqli_stmt_close($stmt);
-        }
-    }
-
-    // Close connection
-    mysqli_close($link);
-}
-?>
-
+<?php include('src/login.php') ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="vendors/bootstrap-4.5.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
@@ -105,21 +16,44 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
           <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
               <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
                   <label>Username</label>
-                  <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                  <input id="username" type="text" name="username" class="form-control" value="<?php echo $username; ?>">
                   <span class="help-block"><?php echo $username_err; ?></span>
               </div>
               <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
                   <label>Password</label>
-                  <input type="password" name="password" class="form-control">
+                  <input id="password" type="password" name="password" class="form-control">
                   <span class="help-block"><?php echo $password_err; ?></span>
               </div>
               <div class="form-group">
-                  <input type="submit" class="btn btn-primary btn-block" value="Login">
+                  <input id="login_btn" type="submit" class="btn btn-primary btn-block" value="Login">
               </div>
               <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
           </form>
         </div>
       </div>
     </div>
+    <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<!-- <script>
+$('#login_btn').click(function() {
+  var username = $('#username').val();
+  var password = $('#password').val();
+  $.ajax({
+        type: "POST",
+        url: "src/login.php",
+        data: {
+          'username': username,
+          'password': password,
+        },
+        success: function(response){
+          alert('Login!');
+        }
+    });
+  });
+
+</script> -->
 </body>
 </html>
--- register.php
+++ register.php
@@ -1,98 +1,4 @@
-<?php
-// Include config file
-require_once "config/config.php";
-
-// Define variables and initialize with empty values
-$username = $password = $confirm_password = "";
-$username_err = $password_err = $confirm_password_err = "";
-
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-
-    // Validate username
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter a username.";
-    } else{
-        // Prepare a select statement
-        $sql = "SELECT user_id FROM users WHERE username = ?";
-
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-
-            // Set parameters
-            $param_username = trim($_POST["username"]);
-
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                /* store result */
-                mysqli_stmt_store_result($stmt);
-
-                if(mysqli_stmt_num_rows($stmt) == 1){
-                    $username_err = "This username is already taken.";
-                } else{
-                    $username = trim($_POST["username"]);
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-
-            // Close statement
-            mysqli_stmt_close($stmt);
-        }
-    }
-
-    // Validate password
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter a password.";
-    } elseif(strlen(trim($_POST["password"])) < 6){
-        $password_err = "Password must have atleast 6 characters.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
-
-    // Validate confirm password
-    if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = "Please confirm password.";
-    } else{
-        $confirm_password = trim($_POST["confirm_password"]);
-        if(empty($password_err) && ($password != $confirm_password)){
-            $confirm_password_err = "Password did not match.";
-        }
-    }
-
-    // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
-
-        // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
-
-            // Set parameters
-            $param_username = $username;
-            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Redirect to login page
-                header("location: login.php");
-            } else{
-                echo "Something went wrong. Please try again later.";
-            }
-
-            // Close statement
-            mysqli_stmt_close($stmt);
-        }
-    }
-
-    // Close connection
-    mysqli_close($link);
-}
-?>
-
+<?php require('src/register.php');  ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -108,23 +14,23 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <div class="my-auto col-md-4 offset-md-4 mt-5">
           <h2 class="pb-4">Sign Up</h2>
           <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-              <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+              <div class="form-group <?php //echo (!empty($username_err)) ? 'has-error' : ''; ?>">
                   <label>Username</label>
-                  <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                  <input id="username" type="text" name="username" class="form-control" value="<?php //echo $username; ?>">
                   <span class="help-block"><?php echo $username_err; ?></span>
               </div>
               <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
                   <label>Password</label>
-                  <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
+                  <input id="password" type="password" name="password" class="form-control" value="<?php //echo $password; ?>">
                   <span class="help-block"><?php echo $password_err; ?></span>
               </div>
               <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
                   <label>Confirm Password</label>
-                  <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
+                  <input type="password" name="confirm_password" class="form-control" value="<?php //echo $confirm_password; ?>">
                   <span class="help-block"><?php echo $confirm_password_err; ?></span>
               </div>
               <div class="form-group">
-                  <input type="submit" class="btn btn-primary btn-block" value="Submit">
+                  <input id="register_btn" type="submit" class="btn btn-primary btn-block" value="Submit">
                   <input type="reset" class="btn btn-default btn-block" value="Reset">
               </div>
               <p>Already have an account? <a href="login.php">Login here</a>.</p>
@@ -132,5 +38,29 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         </div>
       </div>
     </div>
+    <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<script>
+$(document).ready(function(){
+$('#register_btn').click(function() {
+  var username = $('#username').val();
+  var password = $('#password').val();
+  $.ajax({
+        type: "POST",
+        url: "src/register.php",
+        data: {
+          'username': username,
+          'password': password,
+        },
+        success: function(response){
+          alert('Registered!');
+        }
+    });
+  });
+});
+</script>
 </body>
 </html>
