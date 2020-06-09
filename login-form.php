a9926e86c44be8dac1f00018e083ef8fa46e1bc9
Fabio Monzani <renderbros@gmail.com>
2020-06-09 16:45:28 +0100

    Separating login and register pages from logic

---
+++ login-form.php
@@ -0,0 +1,36 @@
+<?php include('login.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+      <div class="row h-100">
+        <div class="my-auto col-md-4 offset-md-4 mt-5">
+          <h2 class="pb-4">Login</h2>
+          <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+              <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                  <label>Username</label>
+                  <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                  <span class="help-block"><?php echo $username_err; ?></span>
+              </div>
+              <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                  <label>Password</label>
+                  <input type="password" name="password" class="form-control">
+                  <span class="help-block"><?php echo $password_err; ?></span>
+              </div>
+              <div class="form-group">
+                  <input type="submit" class="btn btn-primary btn-block" value="Login">
+              </div>
+              <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+          </form>
+        </div>
+      </div>
+    </div>
+</body>
+</html>
